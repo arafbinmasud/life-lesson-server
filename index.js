b78c51d81d4bd0455695c9d1ed289388dd43abb1
arafbinmasud <araf357300@gmail.com>
2026-05-13 15:18:00 +0600

    feat(auth): store user data in database on registration

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require('cors')
+const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.get('/', (req, res) => {
-  res.send('Life Lesson Server is Running!')
-})
+// middlewares 
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+  res.send("Life Lesson Server is Running!");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ci4q50w.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    const db = client.db("life-lesson-db");
+    const usersCollection = db.collection("users");
+
+    // user related apis:
+    app.post("/users", async (req, res) => {
+      const user = {...req.body, createdAt: new Date()};
+      const email = user.email;
+      const query = { email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User Exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
